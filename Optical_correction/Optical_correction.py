8d1864ec7ba68a13ad2debc46007b1257f28b783
Ryan Fullarton <ryan.fullarton@nhs.net>
2023-03-01 14:38:05 +0000

    wrap the corrections in an overall file and edit each one so that its a function within the script

---
+++ Optical_correction/Optical_correction.py
@@ -0,0 +1,39 @@
+import numpy as np
+import cv2 as cv 
+import glob
+import sys
+import os
+from undistort_image import undistort_function
+from vignetting_correction import vingetting_correction
+#from projection_correction import projection_correction
+#from refraction_correction import refraction_correction
+
+
+def main(image_folder, format, calibration_matrix, distortion_coefficients):
+    
+    corrected_image_path = image_folder + 'optical_corrected/'
+    try:
+        os.mkdir(corrected_image_path)
+    except FileExistsError:
+        pass
+
+    image_files = glob.glob(image_folder + '*' + format)
+    for file in image_files:
+        image = np.load(file)
+        image = np.array(image,dtype=np.uint16)
+        filename = file.split('/')[-1][:-len(format)]
+        #image = projection_correction(image)
+        #image = refraction_correction(image)
+        image = vingetting_correction(image, calibration_matrix)
+        image = undistort_function(image, calibration_matrix, distortion_coefficients,1)
+        np.save(corrected_image_path + filename, image)
+
+
+if len(sys.argv) < 5 or len(sys.argv) > 5:
+    print("Please enter the following arguments: \n"
+    "1) Folder containing the image_names \n"
+    "2) Format (extension) of images requiring correction \n"
+    "3) Camera calibration matrix (npy file)\n"
+    "4) Camera calibration distortion coefficients (npy file)")
+else:
+    main(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
---
+++ Optical_correction/undistort_image.py
@@ -0,0 +1,52 @@
+import numpy as np
+import cv2 as cv
+import sys
+import os
+
+
+def undistort_function(img, calibration_matrix, dist_coeffs, crop):
+    #load in calibration matrix and distortion coefficients
+    mtx = np.load(calibration_matrix)
+    dist = np.load(dist_coeffs)
+
+    #get shape of distorted image
+    h,  w = img.shape[:2]
+
+    # Funtion that opgtimises the camera calibration matrix based on the height of the image
+    # The crop option [0] removes any black space created by undistorting
+    newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), crop, (w,h))
+
+    # undistort
+    dst = cv.undistort(img, newcameramtx, dist, None, newcameramtx)
+    return dst
+
+def main(input_image_path, calibration_matrix, dist_coeffs, crop):
+    #load in image as numpy array and convert to unsigned int for grayscale image
+    img = np.load(input_image_path)
+    #img = np.uint8(img)
+    #img = cv.imread(input_image_path)
+    dst = undistort_function(img,calibration_matrix,dist_coeffs,crop)
+    path_split = input_image_path.split("/")
+
+    save_path = "/".join(path_split[:-1]) +"/"
+
+    save_path = save_path + 'undistorted_images/'
+    try:
+        os.mkdir(save_path)
+    except FileExistsError:
+        pass
+    np.save(save_path  + path_split[-1][:-4] + '_undistorted', dst)
+
+
+"""
+
+if len(sys.argv) < 4:
+    print('Please enter the following arguments:\n'
+        '1) Path to distorted image\n'
+        '2) Path to calibration matrix\n'
+        '3) Path to distortion coefficients\n'
+        '4) Whether to crop the undistorted image to remove black space [0 or 1]\n')
+
+else:
+    main(sys.argv[1], sys.argv[2],sys.argv[3], int(sys.argv[4]))
+"""
