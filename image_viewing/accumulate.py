6b3c31da549d7bd9b0e95761773ed6181874db50
Ryan Fullarton <ryan.fullarton@nhs.net>
2023-06-06 16:08:49 +0100

    add script for accumulating npy images

---
+++ image_viewing/accumulate.py
@@ -0,0 +1,18 @@
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+
+file_list = glob.glob(sys.argv[1] + 'acquisition*' )
+bg = np.load(sys.argv[1]+'Background.npy')
+first = np.load(file_list[0])
+
+final = np.zeros(first.shape)
+
+for file in file_list[0:150]:
+    arr = np.load(file)
+    final = final + arr - bg
+np.save(sys.argv[1]+ 'accummulated.npy', final)
+plt.imshow(final, cmap ='gray')
+plt.show()
